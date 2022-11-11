0d737333c4ef1449813ee8052c99b614e4b3fd58
Sohom Pal <sohom377@gmail.com>
2022-11-11 10:53:42 +0530

    use server side components

--- app/todos/[todoId]/page.tsx
+++ app/todos/[todoId]/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Todo } from "../../../typings";
 
 type PageProps = {
   params: {
@@ -6,8 +7,31 @@ type PageProps = {
   };
 };
 
-function TodoPage({ params: { todoId } }: PageProps) {
-  return <div>TodoPage: {todoId}</div>;
+const fetchTodo = async (todoId: string) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoId}`
+  );
+
+  const todo: Todo = await res.json();
+  return todo;
+};
+
+async function TodoPage({ params: { todoId } }: PageProps) {
+  const todo = await fetchTodo(todoId);
+
+  return (
+    <div className="p-10 bg-yellow-200 border-2 m-2 shadow-lg">
+      <p>
+        #{todo.id}: {todo.title}
+      </p>
+
+      <p>Completed: {todo.completed ? "Yes" : "No"}</p>
+
+      <p className="border-t border-black mt-5 text-right">
+        By user: {todo.userId}
+      </p>
+    </div>
+  );
 }
 
 export default TodoPage;
